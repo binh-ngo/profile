bad6a901ed41efcd80b0287e786d402145f3fbf2
Binh Ngo <bnngo97@gmail.com>
2023-03-21 12:05:18 -0700

    added storage stack and db

---
+++ sst.config.ts
@@ -0,0 +1,15 @@
+import { SSTConfig } from "sst";
+// @ts-ignore - module does not provide typing 
+import { StorageStack } from "./stacks/StorageStack";
+
+export default {
+  config(_input) {
+    return {
+      name: "notes",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(StorageStack);
+  },
+} satisfies SSTConfig;
